3a0cacb272ebefa5d4f0e22be86f954b60a84e9f
klaypso <klaypso@proton.me>
2022-01-26 20:34:23 +0000

    fix in readme

--- matlab/caffe/matcaffe_batch.m
+++ matlab/caffe/matcaffe_batch.m
@@ -24,4 +24,52 @@ if nargin < 1
 end
 if ischar(list_im)
     %Assume it is a file contaning the list of images
-    filename 
+    filename = list_im;
+    list_im = read_cell(filename);
+end
+% Adjust the batch size and dim to match with models/bvlc_reference_caffenet/deploy.prototxt
+batch_size = 10;
+dim = 1000;
+disp(list_im)
+if mod(length(list_im),batch_size)
+    warning(['Assuming batches of ' num2str(batch_size) ' images rest will be filled with zeros'])
+end
+
+% init caffe network (spews logging info)
+if exist('use_gpu', 'var')
+  matcaffe_init(use_gpu);
+else
+  matcaffe_init();
+end
+
+d = load('ilsvrc_2012_mean');
+IMAGE_MEAN = d.image_mean;
+
+% prepare input
+
+num_images = length(list_im);
+scores = zeros(dim,num_images,'single');
+num_batches = ceil(length(list_im)/batch_size)
+initic=tic;
+for bb = 1 : num_batches
+    batchtic = tic;
+    range = 1+batch_size*(bb-1):min(num_images,batch_size * bb);
+    tic
+    input_data = prepare_batch(list_im(range),IMAGE_MEAN,batch_size);
+    toc, tic
+    fprintf('Batch %d out of %d %.2f%% Complete ETA %.2f seconds\n',...
+        bb,num_batches,bb/num_batches*100,toc(initic)/bb*(num_batches-bb));
+    output_data = caffe('forward', {input_data});
+    toc
+    output_data = squeeze(output_data{1});
+    scores(:,range) = output_data(:,mod(range-1,batch_size)+1);
+    toc(batchtic)
+end
+toc(initic);
+
+if exist('filename', 'var')
+    save([filename '.probs.mat'],'list_im','scores','-v7.3');
+end
+
+
+

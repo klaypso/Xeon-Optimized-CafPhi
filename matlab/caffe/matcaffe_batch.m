3ef1a6bf3c67d4d3c9986b9a1bf153b86a02ec8f
klaypso <klaypso@proton.me>
2022-01-26 19:14:42 +0000

    initial edits

---
+++ matlab/caffe/matcaffe_batch.m
@@ -0,0 +1,27 @@
+function [scores,list_im] = matcaffe_batch(list_im, use_gpu)
+% scores = matcaffe_batch(list_im, use_gpu)
+%
+% Demo of the matlab wrapper using the ILSVRC network.
+%
+% input
+%   list_im  list of images files
+%   use_gpu  1 to use the GPU, 0 to use the CPU
+%
+% output
+%   scores   1000 x num_images ILSVRC output vector
+%
+% You may need to do the following before you start matlab:
+%  $ export LD_LIBRARY_PATH=/opt/intel/mkl/lib/intel64:/usr/local/cuda/lib64
+%  $ export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libstdc++.so.6
+% Or the equivalent based on where things are installed on your system
+%
+% Usage:
+%  scores = matcaffe_batch({'peppers.png','onion.png'});
+%  scores = matcaffe_batch('list_images.txt', 1);
+if nargin < 1
+  % For test purposes
+  list_im = {'peppers.png','onions.png'};
+end
+if ischar(list_im)
+    %Assume it is a file contaning the list of images
+    filename 

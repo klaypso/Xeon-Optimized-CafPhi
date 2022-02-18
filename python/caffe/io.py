Fix rotation

python/caffe/io.py
@@ -21,4 +21,121 @@ def blobproto_to_array(blob, return_diff=False):
     """
     if return_diff:
         return np.array(blob.diff).reshape(
-            blob.num, blo
+            blob.num, blob.channels, blob.height, blob.width)
+    else:
+        return np.array(blob.data).reshape(
+            blob.num, blob.channels, blob.height, blob.width)
+
+
+def array_to_blobproto(arr, diff=None):
+    """Converts a 4-dimensional array to blob proto. If diff is given, also
+    convert the diff. You need to make sure that arr and diff have the same
+    shape, and this function does not do sanity check.
+    """
+    if arr.ndim != 4:
+        raise ValueError('Incorrect array shape.')
+    blob = caffe_pb2.BlobProto()
+    blob.num, blob.channels, blob.height, blob.width = arr.shape;
+    blob.data.extend(arr.astype(float).flat)
+    if diff is not None:
+        blob.diff.extend(diff.astype(float).flat)
+    return blob
+
+
+def arraylist_to_blobprotovecor_str(arraylist):
+    """Converts a list of arrays to a serialized blobprotovec, which could be
+    then passed to a network for processing.
+    """
+    vec = caffe_pb2.BlobProtoVector()
+    vec.blobs.extend([array_to_blobproto(arr) for arr in arraylist])
+    return vec.SerializeToString()
+
+
+def blobprotovector_str_to_arraylist(str):
+    """Converts a serialized blobprotovec to a list of arrays.
+    """
+    vec = caffe_pb2.BlobProtoVector()
+    vec.ParseFromString(str)
+    return [blobproto_to_array(blob) for blob in vec.blobs]
+
+
+def array_to_datum(arr, label=0):
+    """Converts a 3-dimensional array to datum. If the array has dtype uint8,
+    the output data will be encoded as a string. Otherwise, the output data
+    will be stored in float format.
+    """
+    if arr.ndim != 3:
+        raise ValueError('Incorrect array shape.')
+    datum = caffe_pb2.Datum()
+    datum.channels, datum.height, datum.width = arr.shape
+    if arr.dtype == np.uint8:
+        datum.data = arr.tostring()
+    else:
+        datum.float_data.extend(arr.flat)
+    datum.label = label
+    return datum
+
+
+def datum_to_array(datum):
+    """Converts a datum to an array. Note that the label is not returned,
+    as one can easily get it by calling datum.label.
+    """
+    if len(datum.data):
+        return np.fromstring(datum.data, dtype = np.uint8).reshape(
+            datum.channels, datum.height, datum.width)
+    else:
+        return np.array(datum.float_data).astype(float).reshape(
+            datum.channels, datum.height, datum.width)
+
+
+## Pre-processing
+
+class Transformer:
+    """
+    Transform input for feeding into a Net.
+
+    Note: this is mostly for illustrative purposes and it is likely better
+    to define your own input preprocessing routine for your needs.
+
+    Take
+    net: a Net for which the input should be prepared
+    """
+    def __init__(self, inputs):
+        self.inputs = inputs
+        self.transpose = {}
+        self.channel_swap = {}
+        self.raw_scale = {}
+        self.mean = {}
+        self.input_scale = {}
+
+
+    def __check_input(self, in_):
+        if in_ not in self.inputs:
+            raise Exception('{} is not one of the net inputs: {}'.format(
+                in_, self.inputs))
+
+
+    def preprocess(self, in_, data):
+        """
+        Format input for Caffe:
+        - convert to single
+        - resize to input dimensions (preserving number of channels)
+        - transpose dimensions to K x H x W
+        - reorder channels (for instance color to BGR)
+        - scale raw input (e.g. from [0, 1] to [0, 255] for ImageNet models)
+        - subtract mean
+        - scale feature
+
+        Take
+        in_: name of input blob to preprocess for
+        data: (H' x W' x K) ndarray
+
+        Give
+        caffe_in: (K x H x W) ndarray for input to a Net
+        """
+        self.__check_input(in_)
+        caffe_in = data.astype(np.float32, copy=False)
+        transpose = self.transpose.get(in_)
+        channel_swap = self.channel_swap.get(in_)
+        raw_scale = self.raw_scale.get(in_)
+        mean = self.mean.get(in_
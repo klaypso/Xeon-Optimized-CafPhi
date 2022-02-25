e41d2efc63fd8d4688693e289c7026a6646da759
klaypso <klaypso@proton.me>
2022-02-25 05:53:47 +0000

    )

---
+++ scripts/travis/travis_install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# This script must be run with sudo.
+
+set -e
+
+MAKE="make --jobs=$NUM_THREADS"
+
+# Install apt packages where the Ubuntu 12.04 default and ppa works for Caffe
+
+# This ppa is for gflags and glog
+add-apt-repository -y ppa:tuleu/precise-backports
+apt-get -y update
+apt-get install \
+    wget git curl \
+    python-dev python-numpy \
+    libleveldb-dev libsnappy-dev libopencv-dev \
+    libboost-dev libboost-system-dev libboost-python-dev libboost-thread-dev \
+    libprotobuf-dev protobuf-compiler \
+    libatlas-dev libatlas-base-dev \
+    libhdf5-serial-dev libgflags-dev libgoogle-glog-dev \
+    bc
+
+# Add a special apt-repository to install CMake 2.8.9 for CMake Caffe build,
+# if needed.  By default, Aptitude in Ubuntu 12.04 installs CMake 2.8.7, but
+# Caffe requires a minimum CMake version of 2.8.8.
+if $WITH_CMAKE; then
+  add-apt-repository -y ppa:ubuntu-sdk-team/ppa
+  apt-get -y update
+  apt-get -y install cmake
+fi
+
+# Install CUDA, if needed
+if $WITH_CUDA; then
+  CUDA_URL=http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1204/x86_64/cuda-repo-ubuntu1204_6.5-14_amd64.deb
+  CUDA_FILE=/tmp/cuda_install.deb
+  curl $CUDA_URL -o $CU

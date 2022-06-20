dab28b5d375dab401f12ae48065bdb71b6849b57
klaypso <klaypso@proton.me>
2022-06-20 18:20:37 +0000

    msm: vidc: add new control for limiting i/p buffer size

---
+++ src/caffe/layers/window_data_layer.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/highgui/highgui_c.h>
+#include <stdint.h>
+
+#include <algorithm>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "caffe/common.hpp"
+#include "caffe/data_layers.hpp"
+#include "caffe/layer.hpp"
+#include "caffe/util/benchmark.hpp"
+#include "caffe/util/io.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/util/rng.hpp"
+
+// caffe.proto > LayerParameter > WindowDataParameter
+//   'source' field specifies the window_file
+//   'crop_size' indicates the desired warped size
+
+namespace caffe {
+
+template <typename Dtype>
+WindowDataLayer<Dtype>::~WindowDataLayer<D

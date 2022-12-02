49d46c2e1b48c532170054bb79875ad042d015d4
klaypso <klaypso@proton.me>
2022-12-02 15:34:39 +0000

    More TODO

---
+++ src/caffe/test/test_threshold_layer.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "caffe/blob.hpp"
+#include "caffe/common.hpp"
+#include "caffe/filler.hpp"
+#include "caffe/vision_layers.hpp"
+
+#include "caffe/test/test_caffe_main.hpp"
+
+namespace caffe {
+
+template <typename TypeParam>
+class ThresholdLayerTest : public MultiDeviceTest<TypeParam> {
+  typedef typename TypeParam::Dtype Dtype;
+ pro
